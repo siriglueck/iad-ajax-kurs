c2b1e7121a83613fd62a6c75a8eabe3c3b23e238
siriluck.rattanapong@gmail.com <siriluck.rattanapong@gmail.com>
2025-07-08 13:55:16 +0200

    complete lesson 2

--- js/mehr-ajax-text.js
+++ js/mehr-ajax-text.js
@@ -1,22 +1,33 @@
 var resObj = erzXHRObjekt();
 
 function sndReq(i) {
+  resObj.onreadystatechange = () => {
+    handleResponse(i);
+  };
 
+  resObj.open("get", "info" + i + ".txt", true);
+  resObj.send(null);
 }
 
 function handleResponse(i) {
-
+  if (resObj.status === 200 && resObj.readyState === 4) {
+    document.getElementById("info" + i).innerHTML = resObj.responseText;
+  }
 }
 
-function out() {
-
+function out(i) {
+  document.getElementById("info" + i).innerHTML = "";
 }
 
 function init() {
   const images = document.getElementsByTagName("img");
-  for( let i = 0; i < images; i++) {
-    images[i].onmouseover = () => { sndReq(i); }
-    images[i].onmouseout = out;
+  for (let i = 0; i < images.length; i++) {
+    images[i].onmouseover = () => {
+      sndReq(i);
+    };
+    images[i].onmouseout = () => {
+      out(i);
+    };
   }
 }
 
