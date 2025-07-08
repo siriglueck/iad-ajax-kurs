75a194edb2d8af0bc24e59da1ac28d1124eb74d3
siriluck.rattanapong@gmail.com <siriluck.rattanapong@gmail.com>
2025-07-08 13:26:59 +0200

    complete lesson 1

--- js/ajaxtext.js
+++ js/ajaxtext.js
@@ -1,17 +1,35 @@
+// 1. Erzeuge XHR-Objekt
 let resObj = erzXHRObjekt();
 
 function sndReq() {
-    resObj.open( 'get', 'antwort.txt', true );
-    resObj.onreadystatechange = handleResponse;
-    resObj.send( null );
+  // 2. Callback-Funktion registrieren
+  resObj.onreadystatechange = handleResponse;
+
+  // 3. Verbindung
+  // true = async
+  resObj.open("get", "antwort.txt", true);
+
+  // 4. Anfrage absenden und auf Antwort warten
+  // Das Parameter der send()-Methode ist beim Versenden mit get immer null
+  resObj.send(null);
 }
 
 function handleResponse() {
-    
+  // 5.Verarbeitung der Antwort des Servers
+  // console.log(resObj);
+
+  if(resObj.status === 200 && resObj.readyState === 4) {
+    let antwort = document.querySelector("#antwort");
+    antwort.innerHTML = resObj.responseText;
+
+    antwort.innerHTML += "<hr>Status: " + resObj.status;
+    antwort.innerHTML += "<br>Status-Text: " + resObj.statusText;
+    antwort.innerHTML += "<br>Header-Infos: " + resObj.getAllResponseHeaders();
+  }
 }
 
 function init() {
-    document.getElementById("frage").onclick = sndReq;
+  document.getElementById("frage").onclick = sndReq;
 }
 
 document.addEventListener("DOMContentLoaded", init);
--- js/xhrobject.js
+++ js/xhrobject.js
@@ -1,3 +1,22 @@
 function erzXHRObjekt() {
-    
+  let responseObject = null;
+
+  try {
+    responseObject = new ActiveXObject("Microsoft.XMLHTTP");
+  } catch (e) {
+    try {
+      responseObject = new ActiveXObject("MSXML2.XMLHTTP");
+    } catch (e) {
+      try {
+        responseObject = new XMLHttpRequest();
+      } catch (e) {
+        alert(
+          "Erzeugung des XHR-Objektes fehlgeschlagen.\r\nMeldung: " + e.message
+        );
+        return false;
+      }
+    }
+  }
+
+  return responseObject;
 }
