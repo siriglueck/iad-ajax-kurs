76f58851146f5c55700d45a930aad94058be309d
siriluck.rattanapong@gmail.com <siriluck.rattanapong@gmail.com>
2025-07-09 14:40:18 +0200

    update exercise 1

--- uebung/uebung1.js
+++ uebung/uebung1.js
@@ -1,7 +1,30 @@
 var resObj = erzXHRObjekt();
 
+function sndReq() {
+  resObj.onreadystatechange = () => {
+    handleResponse();
+  };
+  resObj.open("get", "uebung1.xml", true);
+  resObj.send(null);
+}
+
+function handleResponse() {
+  let daten = null;
+  if (resObj.status === 200 && resObj.readyState === 4) {
+    daten = resObj.responseXML;
+    console.log(resObj);
+    if (daten !== null) {
+        document.querySelector("#info" + i).innerHTML =
+        daten.querySelectorAll("info")[i].firstChild.data;
+    } else {
+      document.querySelector("#debug").innerHTML =
+        "Leider keiner XML-Daten gefunden";
+    }
+  }
+}
+
 function init() {
-    document.getElementById("anfrage").onclick = sndReq;
+  sndReq();
 }
 
 document.addEventListener("DOMContentLoaded", init);
