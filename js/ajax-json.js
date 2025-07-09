fbce45901b4f819c89d6a8d48257374405e35966
siriluck.rattanapong@gmail.com <siriluck.rattanapong@gmail.com>
2025-07-09 12:52:27 +0200

    complete lesson 5-6

--- js/ajax-json.js
+++ js/ajax-json.js
@@ -1,47 +1,48 @@
-var resObj = erzXHRObjekt();
+let resObj = erzXHRObjekt();
 
-function sndReq( i ) {
-    resObj.open( 'get', 'info-json.txt', true );
-    resObj.onreadystatechange = function() {
-        handleResponse( i );
-    }
-    resObj.send( null );
+function sndReq(i) {
+  resObj.open("get", "info-json.txt", true);
+  resObj.onreadystatechange = function () {
+    handleResponse(i);
+  };
+  resObj.send(null);
 }
 
-function handleResponse( i ) {
-    var daten = null;
-    if( resObj.readyState == 4 ) {
-        
-        
-    }
+function handleResponse(i) {
+  let daten = null;
+  if (resObj.readyState == 4) {
+    daten = JSON.parse(resObj.responseText);
+    //console.log(daten);
+    document.getElementById("info" + i).innerHTML = daten.info[i];
+  }
 }
 
 function out() {
-    for( i = 0; i < 3; i++ ) {
-        document.getElementById("info" + i).innerHTML = "";
-    }
+  for (i = 0; i < 3; i++) {
+    document.getElementById("info" + i).innerHTML = "";
+  }
 }
 
 function init() {
-    document.getElementsByTagName("img")[0].onmouseover = function() {
-        sndReq(0);
-    }
-    document.getElementsByTagName("img")[1].onmouseover = function() {
-        sndReq(1);
-    }
-    document.getElementsByTagName("img")[2].onmouseover = function() {
-        sndReq(2);
-    }
-    
-    document.getElementsByTagName("img")[0].onmouseout = function() {
-        out();
-    }
-    document.getElementsByTagName("img")[1].onmouseout = function() {
-        out();
-    }
-    document.getElementsByTagName("img")[2].onmouseout = function() {
-        out();
-    }
+  document.getElementsByTagName("img")[0].onmouseover = function () {
+    sndReq(0);
+  };
+  document.getElementsByTagName("img")[1].onmouseover = function () {
+    sndReq(1);
+  };
+  document.getElementsByTagName("img")[2].onmouseover = function () {
+    sndReq(2);
+  };
+
+  document.getElementsByTagName("img")[0].onmouseout = function () {
+    out();
+  };
+  document.getElementsByTagName("img")[1].onmouseout = function () {
+    out();
+  };
+  document.getElementsByTagName("img")[2].onmouseout = function () {
+    out();
+  };
 }
 
 document.addEventListener("DOMContentLoaded", init);
--- js/daten-senden.js
+++ js/daten-senden.js
@@ -1,52 +1,44 @@
 var resObj = erzXHRObjekt();
 
 function handleResponse() {
-    if( resObj.readyState == 4 ) {
-        document.getElementById("antwort").innerHTML = resObj.responseText;
-    }
+  if (resObj.readyState === 4 && resObj.status === 200) {
+    document.getElementById("antwort").innerHTML = resObj.responseText;
+  }
 }
 
 function sndReqGET() {
-    // Parameter zum Senden in Variable ablegen
-    var params = "user=admin&passwort=geheim";
-    
-    // GET-Verbindung öffnen
-    
+  // Parameter zum Senden in Variable ablegen
+  let params = "user=admin&passwort=geheim";
+
+  // GET-Verbindung öffnen
+  resObj.open("get", "daten-senden.php?" + params, true);
+  resObj.onreadystatechange = handleResponse;
+  resObj.send(null);
 }
 
 function sndReqPOST() {
-    // Parameter zum Senden an Variable ablegen
-    
-    
-    // POST-Verbindung öffnen
-    
-    
-    // Header-Info zum Mime-Type der gesendeten Parameter festlagen
-    
-    
-    // Header-Info zur Länge der gesendeten Daten festlegen
-    
-    
-    // Funktion referenzieren bei Änderung des Status der Server-Antwort
-    
-    
-    // Daten mit angehängten Parametern senden
-    
+  // Parameter zum Senden an Variable ablegen
+  let params = "blz=90150001&ktonr=471107";
+
+  // POST-Verbindung öffnen
+  resObj.open("post", "daten-senden.php", true);
+
+  // Header-Info zum Mime-Type der gesendeten Parameter festlagen
+  resObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  // Header-Info zur Länge der gesendeten Daten festlegen
+  //resObj.setRequestHeader("Content-length", params.length);
+
+  // Funktion referenzieren bei Änderung des Status der Server-Antwort
+  resObj.onreadystatechange = handleResponse;
+
+  // Daten mit angehängten Parametern senden
+  resObj.send(params);
 }
 
 function init() {
-    document.getElementById("sendeget").onclick = sndReqGET;
-    document.getElementById("sendepost").onclick = sndReqPOST;
+  document.getElementById("sendeget").onclick = sndReqGET;
+  document.getElementById("sendepost").onclick = sndReqPOST;
 }
 
 document.addEventListener("DOMContentLoaded", init);
-
-
-
-
-
-
-
-
-
-
